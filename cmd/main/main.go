2dfdf3dd80f3c001e40b3987ffef76ebae03803e
Ilia Panfilov <illiia0921@gmail.com>
2023-05-17 21:21:16 +0300

    4rds

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,18 @@
 package main
 
-import {
-	"fmt"
-	"net/http"
+import (
 	"log"
-}
+	"net/http"
 
-func main () {
+	"github.com/Kleist47/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 
 }
